4ed022241b467235a0559ac6401188ca10c21cd9
Rahul Saini <rahulsaini2522@gmail.com>
2026-02-02 00:03:08 +0530

    update file

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
 
 function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1>The Zerodha Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-4 p-3 mt-5 ">
+          <img src="images/zerodhaFundhouse.png"  style={{width:"50%"}}/>
+          <p className="text-small text-muted mt-4">
+            Assets Managenent.
+          </p>
+          
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="images/sensibullLogo.svg" style={{width:"50%"}}/>
+          <p className="text-small text-muted mt-4">
+            Options trading platform.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="images/goldenpiLogo.png" style={{width:"50%"}}/>
+          <p className="text-small text-muted mt-4">
+            Bonds trading problems.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="images/streakLogo.png" style={{width:"50%"}}/>
+          <p className="text-small text-muted mt-4">
+            Algo and strategy platform.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="images/smallcaseLogo.png" />
+          <p className="text-small text-muted mt-4">
+            Thematic investing platform.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="images/dittoLogo.png" style={{width:"50%"}}/>
+          <p className="text-small text-muted mt-4">
+            Insurance platform.
+          </p>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Universe;
