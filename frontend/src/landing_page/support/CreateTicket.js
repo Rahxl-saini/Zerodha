980ddb0fe793ac22bc45ebce9d6c3cb52a753ab8
Rahul Saini <rahulsaini2522@gmail.com>
2026-02-02 20:57:35 +0530

    update file

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function CreateTicket() {
-    return ( 
+  return (
     <div className="container">
-            <div className="row p-5 mt-5 mb-5">
-                <h1 className="fs-2">
-                    To create a ticket, select a relevant topic
-                </h1>
-                <div className="col-4 p-5 mt-5 mb-5">
-                    <h4 className=""><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening</h4>
-                </div>
-            </div>
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create a ticket, select a relevant topic</h1>
+        <div className="col-4 p-5 mt-5 mb-5">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening
+          </h4>
+          <a href="">Online Account Opening</a>
+          <br/>
+          <a href="">Offline Account Opening</a>
+          <br/>
+          <a href="">Company, Partnership and HUF Account</a>
+          <br/>
+          <a href="">Opening</a>
+          <br/>
+          <a href="">NRI Account Opening</a>
+          <br/>
+          <a href="">Charges at Zerodha</a>
+          <br/>
+          <a href="">Zerodha IDFC First Bank 3-in-1 Account</a>
+          <br/>
+          <a href="">Getting Started</a><br/>
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
