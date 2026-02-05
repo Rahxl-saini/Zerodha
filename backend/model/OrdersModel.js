cb112adcd785c64107f162053766cf9f8da1f1ce
Rahul Saini <rahulsaini2522@gmail.com>
2026-02-05 20:52:43 +0530

    update file

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -2,3 +2,6 @@ const { model } = require("mongoose");
 
 const { OrdersModel } = require("../schemas/OrdersSchema");
 
+const OrdersModel = new model("order", OrdersSchema);
+
+module.exports = { OrdersModel };
