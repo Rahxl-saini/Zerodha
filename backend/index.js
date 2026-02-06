bbec37f5b29d7e66d519fdd40e36c81c9c03a9c2
Rahul Saini <rahulsaini2522@gmail.com>
2026-02-06 23:43:28 +0530

    update file

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const { HoldingsModel } = require("./model/HoldingsModel");
 
 const { PositionsModel } = require("./model/PositionsModel");
 
+const { OrdersModel } = require("./model/OrdersModel");
+
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
 
@@ -194,6 +196,19 @@ app.get('/allHoldings', async(requestAnimationFrame, res)=>{
 app.get('/allPositions', async(requestAnimationFrame, res)=>{
     let allPositions = await PositionsModel.find({});
     res.json(allPositions);
+});
+
+app.post("/newOrder", async(req, res)=>{
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+
+    newOrder.save();
+
+    res.send("Order saved!");
 });
 
 app.listen(PORT, ()=>{
