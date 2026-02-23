df9682a747d10a223f5ee7355d33f1ec3790bb6c
Rahul Saini <rahulsaini2522@gmail.com>
2026-02-23 22:29:04 +0530

    update file

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -12,19 +12,21 @@ function Team() {
       >
         <div className="col-6 p-3 text-center">
           <img
-            src="images/rahulsaini.jpeg"
+            src="images/nithinKamath.jpg"
             style={{ borderRadius: "100%", width: "50%" }}
           ></img>
-          <h4 className="mt-5">Rahul Saini</h4>
-          <h6>Software Engineer</h6>
+          <h4 className="mt-5">Nithin Kamath</h4>
+          <h6>Founder, CEO</h6>
         </div>
         <div className="col-6 p-3">
           <p>
-            Rahul Saini is currently pursuing Software Engineering and building a
-            full-stack project to solve real-world problems while strengthening
-            his expertise in modern web technologies.
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry. He
+            is a member of the SEBI Secondary Market Advisory Committee (SMAC)
+            and the Market Data Advisory Committee (MDAC).
           </p>
-          <p> Playing badminton is his zen.</p>
+          <p>Playing basketball is his zen.</p>
           <p>
             Connect on{" "}
             <a href="" style={{ textDecoration: "none" }}>
